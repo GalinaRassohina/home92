a5564154e73c0a7a0eab95d68e1378f86826dac4
Galina <gal-rassohina@yandex.ru>
2022-12-15 21:24:32 +0700

    без ошибок

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+int Sum(int min, int max)
+{
+    if (min > max) return 0;
+    int sum = min + Sum(min+1, max);
+    min ++;
+    return sum;
+}
+
+int M = 4;
+int N = 8;
+int result = Sum(M, N);
+Console.WriteLine(result);
